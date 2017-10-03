5d5e16a847e947454679da8f4dce76a9b768ec12
jban11 <jbane11@gmail.com>
2017-10-03 16:06:31 -0400

    Fixed small bug in Fadccherenkov header file. Should work for any machine with the analyzer's directory in the loaded libraries. Also fixed a libraries compiling script. You should be able to use this script to compile all libs at once.

---
+++ replay/libraries/libs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+currentdir=`pwd`
+
+for dir in */; do  echo "$dir";
+echo " "
+echo " "
+echo "Changing to $dir, and compiling the library!"
+cd $currentdir/$dir
+echo " "
+
+make realclean
+
+echo " "
+
+echo " "
+make
+done
+echo " "
+
+echo " "
+echo "Done compiling the library! Changing back to the libraries main dir."
+chmod -Rf 777 libraries/ 
+cd $currentdir
+
+
+
+
+
