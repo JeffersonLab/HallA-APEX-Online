44821837f377b69d1d515fd2f61ce59b9c4c3509
jbane11 <jbane11@gmail.com>
2017-11-03 09:44:10 -0400

    Quick bug fix, for pathways.

--- replay/rootlogon.C
+++ replay/rootlogon.C
@@ -17,7 +17,7 @@
 {
   //Used for setting paths. %s is necessary so that it can be used to Form the paths.
   //This allows changing the directory in only one place for ease of portability.
-  static const char* replay_dir_prefix = "./replay/%s";
+  static const char* replay_dir_prefix = "./%s";
   TString Arch(gSystem->GetBuildArch());
   TString Arch32("linux");
   TString Arch64("linuxx8664gcc");
