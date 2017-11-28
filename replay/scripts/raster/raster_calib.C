4ba1973b9c132295414d7550713909557bf6f174
Tyler Hague <tjhague@gmail.com>
2017-11-27 20:56:54 -0500

    Minor tweaks and typo fixes

--- replay/scripts/raster/raster_calib.C
+++ replay/scripts/raster/raster_calib.C
@@ -20,8 +20,8 @@ void raster_calib(int run){
   int i = 1;
   //TFile *test_file = new TFile(Form("/volatile/halla/triton/tjhague/rootfiles/coinc_test_%d_%d.root",run,i));
 
-  while(!gSystem->AccessPathName(Form("/volatile/halla/triton/tjhague/rfile_rast/coinc_test_%d_%d.root",run,i),kFileExists)){
-    rootfile->Add(Form("/volatile/halla/triton/tjhague/rfile_rast/coinc_test_%d_%d.root",run,i));
+  while(!gSystem->AccessPathName(Format("/volatile/halla/triton/tjhague/rfile_rast/tritium_%d_%d.root",run,i),kFileExists)){
+    rootfile->Add(Format("/volatile/halla/triton/tjhague/rfile_rast/tritium_%d_%d.root",run,i));
     cout << "Added file: coinc_test_" << run << "_" << i << ".root" << endl;
     i=i+1;
   }                      
@@ -35,7 +35,7 @@ void raster_calib(int run){
 
 
   //Create strings that will get the position of the beam at the target from BPM info
-  //Hardcoded numbers are the position of BPMB w.r.t. the target and the distance between the twoBPMs
+  //Hardcoded numbers are the position of BPMB w.r.t. the target and the distance between the two BPMs
   TString targ_x_pos = "(" + arm + ".BPMB.x) + ((2.214) * ((" + arm + ".BPMB.x - " + arm + ".BPMA.x) / (5.131)))";
   TString targ_y_pos = "(" + arm + ".BPMB.y) + ((2.214) * ((" + arm + ".BPMB.y - " + arm + ".BPMA.y) / (5.131)))";
 
@@ -121,21 +121,6 @@ void raster_calib(int run){
 
   //Calculate and display coeffiecients
   //Give instructions on how to update database?
-  Double_t DbpmAx_offset = bpmax_mean - ((r1x_mean*bpmax_rms)/(r1x_rms*kx));
-  Double_t DbpmAx_slope = bpmax_rms/(r1x_rms*kx);
-  Double_t DbpmAy_offset = bpmay_mean - ((r1y_mean*bpmay_rms)/(r1y_rms*ky));
-  Double_t DbpmAy_slope = bpmay_rms/(r1y_rms*ky);
-
-  Double_t DbpmBx_offset = bpmbx_mean - ((r1x_mean*bpmbx_rms)/(r1x_rms*kx));
-  Double_t DbpmBx_slope = bpmbx_rms/(r1x_rms*kx);
-  Double_t DbpmBy_offset = bpmby_mean - ((r1y_mean*bpmby_rms)/(r1y_rms*ky));
-  Double_t DbpmBy_slope = bpmby_rms/(r1y_rms*ky);
-
-  Double_t Dtargx_offset = targx_mean - ((r1x_mean*targx_rms)/(r1x_rms*kx));
-  Double_t Dtargx_slope = targx_rms/(r1x_rms*kx);
-  Double_t Dtargy_offset = targy_mean - ((r1y_mean*targy_rms)/(r1y_rms*ky));
-  Double_t Dtargy_slope = targy_rms/(r1y_rms*ky);
-
   Double_t UbpmAx_offset = bpmax_mean - ((r1x_mean*bpmax_rms)/(r1x_rms*kx));
   Double_t UbpmAx_slope = bpmax_rms/(r1x_rms*kx);
   Double_t UbpmAy_offset = bpmay_mean - ((r1y_mean*bpmay_rms)/(r1y_rms*ky));
@@ -151,11 +136,26 @@ void raster_calib(int run){
   Double_t Utargy_offset = targy_mean - ((r1y_mean*targy_rms)/(r1y_rms*ky));
   Double_t Utargy_slope = targy_rms/(r1y_rms*ky);
 
-  cout << arm << ".URaster.raw2posA = " << UbpmAx_offset << " " << UbpmAy_offset << " " << UbpmAx_slope << " " << UbpmAy_slope << " 0.0 0.0" << endl; 
-  cout << arm << ".URaster.raw2posB = " << UbpmBx_offset << " " << UbpmBy_offset << " " << UbpmBx_slope << " " << UbpmBy_slope << " 0.0 0.0" << endl; 
-  cout << arm << ".URaster.raw2posT = " << Utargx_offset << " " << Utargy_offset << " " << Utargx_slope << " " << Utargy_slope << " 0.0 0.0" << endl << endl; 
+  Double_t DbpmAx_offset = bpmax_mean - ((r1x_mean*bpmax_rms)/(r1x_rms*kx));
+  Double_t DbpmAx_slope = bpmax_rms/(r1x_rms*kx);
+  Double_t DbpmAy_offset = bpmay_mean - ((r1y_mean*bpmay_rms)/(r1y_rms*ky));
+  Double_t DbpmAy_slope = bpmay_rms/(r1y_rms*ky);
+
+  Double_t DbpmBx_offset = bpmbx_mean - ((r1x_mean*bpmbx_rms)/(r1x_rms*kx));
+  Double_t DbpmBx_slope = bpmbx_rms/(r1x_rms*kx);
+  Double_t DbpmBy_offset = bpmby_mean - ((r1y_mean*bpmby_rms)/(r1y_rms*ky));
+  Double_t DbpmBy_slope = bpmby_rms/(r1y_rms*ky);
+
+  Double_t Dtargx_offset = targx_mean - ((r1x_mean*targx_rms)/(r1x_rms*kx));
+  Double_t Dtargx_slope = targx_rms/(r1x_rms*kx);
+  Double_t Dtargy_offset = targy_mean - ((r1y_mean*targy_rms)/(r1y_rms*ky));
+  Double_t Dtargy_slope = targy_rms/(r1y_rms*ky);
+
+  cout << arm << ".Raster.raw2posA = " << UbpmAx_offset << " " << UbpmAy_offset << " " << UbpmAx_slope << " " << UbpmAy_slope << " 0.0 0.0" << endl; 
+  cout << arm << ".Raster.raw2posB = " << UbpmBx_offset << " " << UbpmBy_offset << " " << UbpmBx_slope << " " << UbpmBy_slope << " 0.0 0.0" << endl; 
+  cout << arm << ".Raster.raw2posT = " << Utargx_offset << " " << Utargy_offset << " " << Utargx_slope << " " << Utargy_slope << " 0.0 0.0" << endl << endl; 
   
-  cout << arm << ".DRaster.raw2posA = " << DbpmAx_offset << " " << DbpmAy_offset << " " << DbpmAx_slope << " " << DbpmAy_slope << " 0.0 0.0" << endl; 
-  cout << arm << ".DRaster.raw2posB = " << DbpmBx_offset << " " << DbpmBy_offset << " " << DbpmBx_slope << " " << DbpmBy_slope << " 0.0 0.0" << endl; 
-  cout << arm << ".DRaster.raw2posT = " << Dtargx_offset << " " << Dtargy_offset << " " << Dtargx_slope << " " << Dtargy_slope << " 0.0 0.0" << endl; 
+  cout << arm << ".Raster2.raw2posA = " << DbpmAx_offset << " " << DbpmAy_offset << " " << DbpmAx_slope << " " << DbpmAy_slope << " 0.0 0.0" << endl; 
+  cout << arm << ".Raster2.raw2posB = " << DbpmBx_offset << " " << DbpmBy_offset << " " << DbpmBx_slope << " " << DbpmBy_slope << " 0.0 0.0" << endl; 
+  cout << arm << ".Raster2.raw2posT = " << Dtargx_offset << " " << Dtargy_offset << " " << Dtargx_slope << " " << Dtargy_slope << " 0.0 0.0" << endl; 
 }
