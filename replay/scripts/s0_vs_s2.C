f9dfd941c1262523f836e60513e9e8e95cb42681
HallA-Online <randallm@jlab.org>
2018-01-14 11:45:55 -0500

    local changes
    -histogram tweaks, proton mass tweak

--- replay/scripts/s0_vs_s2.C
+++ replay/scripts/s0_vs_s2.C
@@ -12,10 +12,10 @@ void s0_vs_s2(Int_t spec=0,int plot=0)     //LHRS: spec=0; RHRS: spec=1
   TTree *tree = (TTree*)gDirectory->Get("T");
 
   if(spec==0){
-     tree->SetBranchAddress("L.s2.la",s2la);
-     tree->SetBranchAddress("L.s2.ra",s2ra);
-     tree->SetBranchAddress("L.s0.la",&s0la);
-     tree->SetBranchAddress("L.s0.ra",&s0ra);
+     tree->SetBranchAddress("L.s2.la_c",s2la);
+     tree->SetBranchAddress("L.s2.ra_c",s2ra);
+     tree->SetBranchAddress("L.s0.la_c",&s0la);
+     tree->SetBranchAddress("L.s0.ra_c",&s0ra);
 
      tree->SetBranchAddress("L.cer.asum_c",&Cersum);
      tree->SetBranchAddress("L.prl1.e",&pse);
@@ -25,10 +25,10 @@ void s0_vs_s2(Int_t spec=0,int plot=0)     //LHRS: spec=0; RHRS: spec=1
    }
 
   if(spec==1){
-     tree->SetBranchAddress("R.s2.la",s2la);
-     tree->SetBranchAddress("R.s2.ra",s2ra);
-     tree->SetBranchAddress("R.s0.la",&s0la);
-     tree->SetBranchAddress("R.s0.ra",&s0ra);
+     tree->SetBranchAddress("R.s2.la_c",s2la);
+     tree->SetBranchAddress("R.s2.ra_c",s2ra);
+     tree->SetBranchAddress("R.s0.la_c",&s0la);
+     tree->SetBranchAddress("R.s0.ra_c",&s0ra);
 
      tree->SetBranchAddress("R.cer.asum_c",&Cersum);
      tree->SetBranchAddress("R.ps.e",&pse);
@@ -41,12 +41,12 @@ void s0_vs_s2(Int_t spec=0,int plot=0)     //LHRS: spec=0; RHRS: spec=1
   Double_t s2_adc=0;
   Double_t s0_adc=0;
 
-  TH2F *hs0s2 = new TH2F("hs0s2","s0 adc vs s2 adc",8000,5000,40000,5000,150000,180000);
+  TH2F *hs0s2 = new TH2F("hs0s2","s0 adc vs s2 adc",1000,0,4000,1000,0,4000);
   hs0s2->GetXaxis()->SetTitle("s0_adc");hs0s2->GetXaxis()->CenterTitle();
   hs0s2->GetYaxis()->SetTitle("s2_adc");hs0s2->GetYaxis()->CenterTitle();
 
 
-  TH2F *hs0s2w = new TH2F("hs0s2w","s0 adc vs s2 adc w cut",8000,5000,40000,5000,150000,180000);
+  TH2F *hs0s2w = new TH2F("hs0s2w","s0 adc vs s2 adc w cut",1000,0,4000,1000,0,4000);
   hs0s2w->GetXaxis()->SetTitle("s0_adc");hs0s2w->GetXaxis()->CenterTitle();
   hs0s2w->GetYaxis()->SetTitle("s2_adc");hs0s2w->GetYaxis()->CenterTitle();
 
