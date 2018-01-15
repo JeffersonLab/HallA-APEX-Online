2c3bee7446f6cc4886f3f3f632d984a364d68eeb
mnycz <mnycz@jlab.org>
2018-01-15 18:03:18 -0500

    update

--- replay/def_tritium.h
+++ replay/def_tritium.h
@@ -37,8 +37,7 @@
 //Used for setting paths. %s is necessary so that it can be used to Form the paths.
 //This allows changing the directory in fewer places for ease of portability.
 const char* REPLAY_DIR_PREFIX = "./%s";
-//const char* ROOTFILE_DIR_PREFIX = "./t2root/%s";
-const char* ROOTFILE_DIR_PREFIX = "/chafs1/work1/tritium/%s";
+const char* ROOTFILE_DIR_PREFIX = "./t2root/%s";
 
 typedef struct _sReplaySetUp
 {
@@ -61,7 +60,7 @@ static const char* PATHS[] = {
 static const char* RAW_DATA_FORMAT="%s/triton_%d.dat.%d";
 //static const char* RAW_DATA_FORMAT="%s/dvcs14_%d.dat.%d";
 
-TString STD_REPLAY_OUTPUT_DIR= TString::Format(ROOTFILE_DIR_PREFIX,"M_rootfiles");
+TString STD_REPLAY_OUTPUT_DIR= TString::Format(ROOTFILE_DIR_PREFIX,"Rootfiles");
 TString CUSTOM_REPLAY_OUTPUT_DIR= TString::Format(ROOTFILE_DIR_PREFIX,"ScratchROOTfiles");
 TString SUMMARY_PHYSICS_FORMAT= TString::Format(REPLAY_DIR_PREFIX,"summaryfiles/summaryphy_%d.log");
 
