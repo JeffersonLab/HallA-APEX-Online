4621828039f0bf682699171e66daf0be27b5b2c4
R. Evan McClellan <vanlellan@gmail.com>
2018-02-28 14:47:42 -0500

    added pass1-specific sourceme

---
+++ replay/pass1_sourceme.sh
@@ -0,0 +1,4 @@
+#export LD_LIBRARY_PATH=/adaqfs/home/a-onl/tritium/analyzer:/adaqfs/home/a-onl/evio-4.3/Linux-x86_64/lib/:$ROOTSYS/lib
+#export PATH=/adaqfs/home/a-onl/tritium/analyzer/:$PATH
+export DB_DIR=/adaqfs/home/a-onl/tritium_auto_pass1/replay/DB/
+#export ANALYZER=/adaqfs/home/a-onl/tritium/analyzer/
