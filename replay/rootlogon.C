ca4666df6b7374e7b7d6587b6a6e579eca4fd2bc
ShujieL <shujie@jlab.org>
2018-08-22 19:10:39 -0400

    add fadc library

--- replay/rootlogon.C
+++ replay/rootlogon.C
@@ -31,6 +31,7 @@
     gSystem->Load(Form(replay_dir_prefix,"ReplayCore64_C.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/Tritium_Xscin/libTritium_Xscin.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/TriFadcScin/libTriFadcScin.so"));
+    gSystem->Load(Form(replay_dir_prefix,"libraries/TriFadcXscin/libTriFadcXscin.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/TriFadcCherenkov/libTriFadcCherenkov.so"));
     gSystem->Load(Form(replay_dir_prefix,"libraries/TriFadcShower/libTriFadcShower.so"));
     gSystem->Load(Form("./libraries/Tritium_HRS/libTritium_HRS.so"));
