7aded1af77f01fba56ff5228d83a9be4a5330bf0
Jason Bane <jbane11@gmail.com>
2019-01-25 11:41:26 -0500

    Update ROL file name

--- replay/DB/GetFADCSettings.sh
+++ replay/DB/GetFADCSettings.sh
@@ -2,17 +2,32 @@
 
 
 outfile="./FADCSettings.txt"
+LeftROLfile="fadc_vxs_apex.c"
+RightROLfile="roc20_tritium_list.c"
+
+LeftPath="/adaqfs/home/adaq/fadc_mike/BlkRead/"
+RightPath="/adaqfs/home/adaq/tritium_fadc/BlkRead/"
+
+LeftROL="${LeftPath}""${LeftROLfile}"
+
+RightROL="${RightPath}""${RightROLfile}"
+
+echo ${LeftROL}
+echo ${RightROL}
+
+echo 
+echo
 if [ -e ${outfile} ]
 then
 	rm -v -f $outfile
 fi
 
 echo "Right arm">>$outfile
-grep -n -r "#define FADC_NSA" /adaqfs/home/adaq/tritium_fadc/BlkRead/roc20_tritium_list.c >> $outfile
-grep -n -r "#define FADC_NSB" /adaqfs/home/adaq/tritium_fadc/BlkRead/roc20_tritium_list.c >> $outfile
-grep -n -r "#define FADC_WIN" /adaqfs/home/adaq/tritium_fadc/BlkRead/roc20_tritium_list.c >> $outfile
+grep -n -r "#define FADC_NSA" ${RightROL}>> $outfile
+grep -n -r "#define FADC_NSB" ${RightROL}>> $outfile
+grep -n -r "#define FADC_WIN" ${RightROL}>> $outfile
 
 echo "Left arm">>$outfile
-grep -n -r "#define FADC_NSA" /adaqfs/home/adaq/fadc_mike/BlkRead/roc31_tritium_list.c >> $outfile
-grep -n -r "#define FADC_NSB" /adaqfs/home/adaq/fadc_mike/BlkRead/roc31_tritium_list.c >> $outfile
-grep -n -r "#define FADC_WIN" /adaqfs/home/adaq/fadc_mike/BlkRead/roc31_tritium_list.c >> $outfile
+grep -n -r "#define FADC_NSA" ${LeftROL} >> $outfile
+grep -n -r "#define FADC_NSB" ${LeftROL} >> $outfile
+grep -n -r "#define FADC_WIN" ${LeftROL} >> $outfile
