76615e07d946732d56a39aa7e21fea0e812c021a
Hall A Online <a-onl@jlab.org>
2019-02-25 17:19:04 -0500

    Improved coincidence plots to add proper pid selection and text showing good electron/ positron coincidences

--- replay/scripts/deltaR_deltaL.C
+++ replay/scripts/deltaR_deltaL.C
@@ -19,7 +19,7 @@ void deltaR_deltaL(Int_t flag, TString drawoption){
     // ht1->GetYaxis()->SetTitle("Amplitude [ADC Channel]");ht1->GetYaxis()->CenterTitle();
     // ht1->SetMarkerStyle(3);ht1->SetMarkerSize(0.75);
     // &&(R.ps.e+R.sh.e)/(R.gold.p*1000)>0.7 &&(L.prl1.e+L.prl2.e)/(L.gold.p*1000)>0.7&
-    tree->Draw("R.tr.tg_dp:L.tr.tg_dp>>ht1aa","R.cer.asum_c>2000&&R.tr.n==1&&L.cer.asum_c>2000&&L.tr.n==1&&((DR.rrawt2>1680)&&(DR.rrawt2<1700))&&fEvtHdr.fEvtType==6","COLZ");
+    tree->Draw("R.tr.tg_dp:L.tr.tg_dp>>ht1aa","R.cer.asum_c>1000&&R.tr.n==1&&L.cer.asum_c>1000&&L.tr.n==1&&((DR.rrawt2>1680)&&(DR.rrawt2<1700))&&fEvtHdr.fEvtType==6","COLZ");
   }
   
   	else if(flag==2){
@@ -29,7 +29,7 @@ void deltaR_deltaL(Int_t flag, TString drawoption){
     // ht1->GetYaxis()->SetTitle("Amplitude [ADC Channel]");ht1->GetYaxis()->CenterTitle();
     // ht1->SetMarkerStyle(3);ht1->SetMarkerSize(0.75);
     // &&(R.ps.e+R.sh.e)/(R.gold.p*1000)>0.7 &&(L.prl1.e+L.prl2.e)/(L.gold.p*1000)>0.7&
-    tree->Draw("R.tr.tg_dp+L.tr.tg_dp>>ht1ab","R.cer.asum_c>2000&&R.tr.n==1&&L.cer.asum_c>2000&&L.tr.n==1","COLZ");
+    tree->Draw("R.tr.tg_dp+L.tr.tg_dp>>ht1ab","R.cer.asum_c>1000&&R.tr.n==1&&L.cer.asum_c>1000&&L.tr.n==1","COLZ");
   }
 
 	else if(flag==3){
@@ -39,6 +39,16 @@ void deltaR_deltaL(Int_t flag, TString drawoption){
     // ht1->GetYaxis()->SetTitle("Amplitude [ADC Channel]");ht1->GetYaxis()->CenterTitle();
     // ht1->SetMarkerStyle(3);ht1->SetMarkerSize(0.75);
     // &&(R.ps.e+R.sh.e)/(R.gold.p*1000)>0.7 &&(L.prl1.e+L.prl2.e)/(L.gold.p*1000)>0.7&
-    tree->Draw("R.tr.tg_dp+L.tr.tg_dp>>ht1ac","R.cer.asum_c>2000&&R.tr.n==1&&L.cer.asum_c>2000&&L.tr.n==1&&((DR.rrawt2>1680)&&(DR.rrawt2<1700))&&fEvtHdr.fEvtType==6","COLZ");
+    tree->Draw("R.tr.tg_dp+L.tr.tg_dp>>ht1ac","R.cer.asum_c>1000&&R.tr.n==1&&L.cer.asum_c>1000&&L.tr.n==1&&((DR.rrawt2>1680)&&(DR.rrawt2<1700))&&fEvtHdr.fEvtType==6","COLZ");
+    
+    TLatex *t = new TLatex();
+    t->SetTextColor(kRed);
+    t->SetTextSize(0.04);
+    t->SetTextAlign(12);
+    int i1=ht1ac->Integral();
+    t->DrawLatexNDC(0.5,0.85,Form("good coincidence counts"));
+    t->DrawLatexNDC(0.5,0.80,Form("e+/e- and good coinc. timing: %d",i1));
+
+
   }
 }
