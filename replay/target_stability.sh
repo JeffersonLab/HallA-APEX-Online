95b47a2491a0aaea95e6ff583a6f0d80010be88a
Hall A Online <a-onl@jlab.org>
2019-03-07 21:35:48 -0500

    Updated target stability macro

--- replay/target_stability.sh
+++ replay/target_stability.sh
@@ -4,7 +4,11 @@ initial=$1
 final=$2
 for i in `seq $initial $final`;
 do
-	root -b -l -q ./scripts/target_stability_macro.C\(3,\"\",${i}\) # T2 stability
-	root -b -l -q ./scripts/target_stability_macro.C\(2,\"\",${i}\) # T6 stability
-	root -b -l -q ./scripts/target_stability_macro.C\(1,\"\",${i}\) # integrated charge
+	root -b -l -q ./target_stability_macro.C\(7,\"\",${i}\) # T5/T2 avg value per run
+	root -b -l -q ./target_stability_macro.C\(6,\"\",${i}\) # T6 avg value per run
+	root -b -l -q ./target_stability_macro.C\(5,\"\",${i}\) # T5 avg value per run
+	root -b -l -q ./target_stability_macro.C\(4,\"\",${i}\) # T2 avg value per run
+	root -b -l -q ./target_stability_macro.C\(3,\"\",${i}\) # T2/uA stability
+	root -b -l -q ./target_stability_macro.C\(2,\"\",${i}\) # T6/uA stability
+	root -b -l -q ./target_stability_macro.C\(1,\"\",${i}\) # integrated charge
 done
