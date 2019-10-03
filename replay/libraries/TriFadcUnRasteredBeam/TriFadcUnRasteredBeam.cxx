c53776090e2b5ab75beeac1e08fd6ae8eb2f9a13
john w <80jtaw@gmail.com>
2019-10-03 17:22:29 +0100

    Changed Unrastered beam class such that it takes into account changes of BPMB to target distance for APEX set-up (1.166 m from previous 2.214m)

--- replay/libraries/TriFadcUnRasteredBeam/TriFadcUnRasteredBeam.cxx
+++ replay/libraries/TriFadcUnRasteredBeam/TriFadcUnRasteredBeam.cxx
@@ -62,7 +62,19 @@ Int_t TriFadcUnRasteredBeam::Reconstruct()
 
   // ignore the runningsum calculation for now, may add it back later
   fDirection = p[1]-p[0];
-  fPosition = p[1] + (p[1](2)/(p[0](2)-p[1](2))) * fDirection ;
+
+  // John Williamson: for APEX set-up the distance from BtoT is changed from 2.214 m to 1.166 m, relative distance of BPMs was fixed
+  Double_t TminB = 1.166; // value +ve as = target_z - BPMB_z and BPM_z is 1.166 upstream of target
+
+  // logic:
+  // x = f(x_0) + f'(x_0)*x
+  // => x_t = x_b + slope*(x_t -x_b)
+  // => x_t = x_b + ((f(x_b) - f(x_a))/( x_b - x_a)) * (x_t - x_b) 
+  // where x_b = p[1], fDirection ~ ((f(x_b) - f(x_a)), (p[1](2)-p[2](2)) ~ ( x_b - x_a)  and TminB = (x_t - x_b) 
+
+
+  //  fPosition = p[1] + (p[1](2)/(p[0](2)-p[1](2))) * fDirection ;
+  fPosition = p[1] + (TminB/(p[1](2)-p[0](2))) * fDirection ;
 
   Update();
 
